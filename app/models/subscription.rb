8a226a49c5013469ae817659f974d79b9117225a
yagi0409 <91511994+yagi0409@users.noreply.github.com>
2023-04-29 23:06:24 +0900

    作業中断

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ApplicationRecord
   has_one_attached :image
   attr_accessor :calender_day
 
+  validates :name, presence: true
+
   def next_update_day
     contract_day.since(self.update_cycle.month)
   end
