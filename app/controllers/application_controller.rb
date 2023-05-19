5c0c00f09582187c7e60bb910b176c936e2a2d99
yagi0409 <91511994+yagi0409@users.noreply.github.com>
2023-05-19 22:22:54 +0900

    authenticate_user設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!,except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_subsc_form
   before_action :get_top_subscriptions
 
   private
-
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
     end
