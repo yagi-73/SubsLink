3d450f2a18e4b5908d718b3f26dc38dc1fa7482e
yagi-73 <91511994+yagi0409@users.noreply.github.com>
2023-05-25 12:49:30 +0900

    bootstrap-show-password導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap"
 import "../stylesheets/application" 
 import '@fortawesome/fontawesome-free/js/all'
 import "jquery-validation"
+import "bootstrap-show-password"
 
 import "./validates/user"
 import "./messages_ja"
--- app/javascript/packs/validates/user.js
+++ app/javascript/packs/validates/user.js
@@ -22,6 +22,14 @@ $(document).on('turbolinks:load', function() {
           equalTo: "パスワードが一致していません"
         },
       },
+
+      errorPlacement: function(error, element){
+        if(element.attr('data-toggle')=='password'){
+          element.parents(".form-group").append(error)
+        } else{
+          error.insertAfter(element)
+        }
+      },
       errorClass: "is-invalid",
       validClass: "is-valid",
     });
